8023b3abbadd6d5f4dd12ada58cb2e74a83f9094
Nikita Novikov <gambitrus38@yandex.ru>
2022-05-27 17:19:05 +0300

    ref

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 // Constants
 const number = document.querySelectorAll('.number');
 const mainTitle = document.querySelector('.title');
+const btnCustom = document.querySelector('.btn-customize');
+const customCountdown = document.querySelector('.custom-countdown');
+const customForm = document.querySelector('.customize');
+const customDateValue = document.querySelector('.custom-date');
+const btnClosePopup = document.querySelector('.btn-close-popup');
+const btnSumbit = document.querySelector('.btn-submit');
 
 // Get today date
 const today = new Date();
@@ -35,3 +41,21 @@ function countDown () {
 };
 
 setInterval(countDown, second);
+
+// Listeners----------------------------------------------
+
+// Open Popup
+btnCustom.addEventListener('click', () => customCountdown.classList.toggle('is-active'))
+
+// Close Popup
+btnClosePopup.addEventListener('click', () => {
+  customCountdown.classList.remove('is-active');
+  customDateValue.value = '';
+})
+
+// Submit form with new data
+customForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  console.log(customDateValue.value);
+
+})
